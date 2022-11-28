46ea27bdeb309f1eace3c2ebaf921a6c5b63963c
heath <heath@STEVENSFAMILYPC>
2022-11-28 13:41:43 -0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,33 @@
+//Heather Stevens
+//CIS 1202 R02
+//11/28/2022
+//Week 15 Exceptions
+
+#include<iostream>
+using namespace std;
+
+class invalidCharacterException	//class name for invalid character exception
+{
+
+};
+class invalidRangeException	//class name for invalid range exception
+{
+
+};
+
+
+char character(char start, int offset);	//function prototype
+
+int main()
+{
+
+
+
+	system("pause");
+	return 0;
+}
+
+char character(char s, int off)
+{
+	return s;
+}
